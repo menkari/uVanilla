0bc8c778c1bb35f51404cb4c6bd5748edc2024f5
Matthew Merryfull <matthew@merryfull.com>
2016-03-15 13:00:49 +1100

    bootstrap laid in via gulp runner

---
+++ uVanilla/gulpfile.js
@@ -0,0 +1,20 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+var debug = require('gulp-debug');
+
+// Where is my cheese located?
+var config = {
+    bowerDir: './bower_components',
+    nodePackages: './node_modules',
+    appDir: './'
+};
+
+gulp.task('bootstrap', function () {
+
+    gulp.src(config.appDir + 'scss/bootstrap.scss')
+        .pipe(debug())
+        .pipe(sass({
+            includePaths: [config.appDir]
+        }))
+        .pipe(gulp.dest(config.appDir + 'Content/'))
+});
